c42f5bf312f4ca5e59c2d986ccef81032ac59577
KrajewskiD <87072336+KrajewskiD@users.noreply.github.com>
2026-06-12 08:14:15 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Tailwind CSS działa
+      </h1>
+    </main>
+  );
+} 
+
+export default App
